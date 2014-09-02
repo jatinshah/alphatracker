02e1bf6ce95b267fb85d46f91334207fed82a001
jatinshah <jatindshah@gmail.com>
2014-09-02 21:14:04 +0530

    Added ContentTypeAdmin

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from userprofile.models import UserProfile
+from django.contrib.contenttypes.models import ContentType
 
+
+class ContentTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'app_label']
+    fieldsets = (
+        ('', {
+            'classes': ('',),
+            'fields': ('name', 'app_label')
+        }),
+    )
+
+
+admin.site.register(ContentType, ContentTypeAdmin)
 # Register your models here.
 admin.site.register(UserProfile)
