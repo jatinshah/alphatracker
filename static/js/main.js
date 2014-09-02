4225011363a864a9aa51310c0e8a004de0952d6a
jatinshah <jatindshah@gmail.com>
2014-09-02 23:09:20 +0530

    Checking typeahead with local states list

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,21 @@
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+var states = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // `states` is an array of state names defined in "The Basics"
+    local: $.map(states, function(state) { return { value: state }; })
+});
+
 var stocks = new Bloodhound({
     datumTokenizer: function (data) {
         var stock_name = Bloodhound.tokenizers.whitespace(data.name);
@@ -10,7 +28,10 @@ var stocks = new Bloodhound({
     },
     limit: 10
 });
+
+
 stocks.initialize();
+states.initialize();
 
 $('#stock-symbol .typeahead').typeahead(
     {
@@ -18,11 +39,15 @@ $('#stock-symbol .typeahead').typeahead(
         highlight: true,
         minLength: 1
     },
+//    {
+//        name: 'stocks',
+//        displayKey: 'symbol',
+//        source: stocks.ttAdapter(),
+//        templates: {
+//            suggestion: Handlebars.compile('<p>{{symbol}} - {{name}}</p>')
+//        }
     {
-        name: 'stocks',
-        displayKey: 'symbol',
-        source: stocks.ttAdapter(),
-        templates: {
-            suggestion: Handlebars.compile('<p>{{symbol}} - {{name}}</p>')
-        }
+        name: 'states',
+        displayKey: 'value',
+        source: states.ttAdapter()
     });
